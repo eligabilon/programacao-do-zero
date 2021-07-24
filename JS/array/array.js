4accaa95d1346ccc71712ecab32a5a2ba0fa3a6b
Eli <eligabilon@gmail.com>
2021-07-23 22:40:23 -0300

    colocado filtro no array

--- JS/array/array.js
+++ JS/array/array.js
@@ -7,6 +7,9 @@ carros.splice(posicao, 1) // remove uma posição do meu array
 
 carros.push("brasilia"); // insere a *brasilia* na última posição do meu array
 
+
+console.log("Meu carro: " + carros.filter("corsa"));
+
 console.log(carros);
 
 carros.pop()
