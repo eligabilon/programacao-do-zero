4fee8ab05d7fb4324dede8dd7312c28b65efd919
Eli <eligabilon@gmail.com>
2021-07-24 11:26:41 -0300

    atividade de números pares

---
+++ JS/numeros_pares/par.js
@@ -0,0 +1,13 @@
+var n = 8;//prompt("Digite um numero");
+
+var total=n/2;
+
+if(n%2 == 0){
+	//alert("Par");
+	console.log("Par");
+} else {
+	//alert("Impar");
+	console.log("Impar");
+}
+
+console.log(total);
