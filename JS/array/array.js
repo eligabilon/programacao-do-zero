78b0da65e60b36711ddcc7d3e37ec067e4fe4677
Eli <eligabilon@gmail.com>
2021-07-23 21:51:03 -0300

    Arquivo sobre array

---
+++ JS/array/array.js
@@ -0,0 +1,17 @@
+              // 0         1      2      3       4       5        6       7    
+var carros = ['fusca', 'corsa', 'bmw', 'audi', 's10', 'ranger', 'gol', 'hilux']
+var carros2 = ['carroça', 'carro de boi', 'carro de chinês']
+
+var posicao = carros.indexOf('audi') // busco por indice
+carros.splice(posicao, 1) // remove uma posição do meu array
+
+carros.push("brasilia"); // insere a *brasilia* na última posição do meu array
+
+console.log(carros);
+
+carros.pop()
+
+console.log(carros);
+
+
+
