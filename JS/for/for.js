5edd9bc678adde7d46ea7fe651a4d87a9b794ae2
CarlosEdu4002 <eduardocadu4002@gmail.com>
2021-07-23 22:03:48 -0300

    codigo for

---
+++ JS/for/for.js
@@ -0,0 +1,16 @@
+var arrayy = ["fusca", "bmw", "corsa", "hilux", "toro"];
+
+for (var i = 0; i <= arrayy.length; i++) {
+    if(arrayy[i] === 'bmw'){
+        console.log(`eu quero ${arrayy[i]}`, i);
+    }
+    } console.log(arrayy[i]);
+}
+
+
+var i = 0;
+var arrayy = ["fusca", "bmw", "corsa", "hilux", "toro"];
+
+while (i <= arrayy.length) {
+    console.log(arrayy[i]);
+    i++;
