3d459de71676458c7769b29e641f6b68ee110198
Eli <eligabilon@gmail.com>
2021-07-23 22:42:45 -0300

    coloquei espaço e removi uma linha

--- JS/array/array.js
+++ JS/array/array.js
@@ -2,11 +2,10 @@
 var carros = ['fusca', 'corsa', 'bmw', 'audi', 's10', 'ranger', 'gol', 'hilux']
 var carros2 = ['carroça', 'carro de boi', 'carro de chinês']
 
-var posicao = carros.indexOf('audi') // busco por indice
+var posicao = carros.indexOf('audi') // busco por indice 
 carros.splice(posicao, 1) // remove uma posição do meu array
 
 carros.push("brasilia"); // insere a *brasilia* na última posição do meu array
-
 
 console.log("Meu carro: " + carros.filter("corsa"));
 
