5b2244f5305c08c04a693af90df638e8cbe43757
CarlosEdu4002 <eduardocadu4002@gmail.com>
2021-08-07 23:06:46 -0300

    testando aula sobre DOM

---
+++ Testes DOM/testejs.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+
+    var titulo = document.getElementById("titule")
+    titulo.style.backgroundColor = "red"
+/////////////////////////////////////////////////////
+    var texto = document.getElementById("lorem")
+    texto.style.backgroundColor = "black"
+    texto.style.color = "chartreuse"
+    texto.style.textAlign = "center"
+////////////////////////////////////////////////////
+    var corpo = document.getElementById("corpo")
+    corpo.style.backgroundColor = "black"
+    
+}
+
