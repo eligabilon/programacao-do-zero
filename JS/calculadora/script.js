618a19c3fa26ed99b2b5145e58801d7d21a2774b
Eli <eligabilon@gmail.com>
2021-09-15 23:01:49 -0300

    alteração no código da calculadora

--- JS/calculadora/script.js
+++ JS/calculadora/script.js
@@ -1,27 +1,26 @@
-var a = '';
-var b = '';
-var num = []; 
+var a = ""; //usado para mostrar na tela div 'screen' o que foi digitado
+var b = "";
+var num = []; //usado para guardar todos os valores digitados
 
-function sendChar(digit) {
-    
-    num.push(digit);
+function sendChar(digit) { //função que recebe o que foi digitado no html
+  num.push(digit); //coloco valores dentro do meu array
 
-    if (num.length != 1) {
-        a = '';
-        document.getElementById("screen").innerHTML = a;
-    }
-    
-    for(i=0; i<num.length; i++){
-        a = a + num[i];
-    }
+  if (num.length != 1) { //caso a qtd seja diferente de 1 limpo a minha var 'a'
+    a = "";
+    document.getElementById("screen").innerHTML = a; //limpo a minha div
+  }
 
-    document.getElementById("screen").innerHTML = a;
+  for (i = 0; i < num.length; i++) { //faço um for para pegar todos os itens do meu array
+    a = a + num[i]; //pego item a item e coloco na variavel 'a'
+  }
+
+  document.getElementById("screen").innerHTML = a; //imprimo na tela div 'screen' o valor de 'a', no caso o que foi digitado
 }
 
-function equalTo() {
+function equalTo() { //função que calcula o que foi digitado
 
 }
 
-function clear() {
+function clearAC() { //função que limpa os valores apresentados na div 'screen'
 
 }
