eb7af63e2975e880dc2d2e38c0f85af2c9a72f86
Eli Gabilon <eligabilon@gmail.com>
2021-11-03 22:59:13 -0300

    api-node

--- api-node/app.js
+++ api-node/app.js
@@ -1,79 +1,106 @@
 const http = require("http");
 const express = require("express");
 const { response } = require("express");
-const { v4 } = require("uuid");
-var cors = require("cors");
-
+const { v4 } =require("uuid");
 const app = express();
-app.use(cors());
 
 app.use(express.json());
 
+//simulando um banco de dados com um array de obj
 const todos = [
   {
-    text: "Fazer compra",
+    text: "Fazer compras",
     done: true,
+    id: v4(),
+    user:{
+      name: "eli",
+      password: "kj3ldk3==3k4",
+      mail: "eligabilon@gmail.com"
+    }
+  },
+  {
+    text: "Levar o carro no mecânico",
+    done: false,
+    id: v4(),
+  },
+  {
+    text: "Levar louça",
+    done: false,
     id: v4(),
   },
 ];
 
-app.get("/todos", (request, response) => {
+//GET - Listar todos os registros
+app.get("/todos", (request, response) =>{
+console.log("GET-todos");
   return response.json(todos);
 });
 
+//GET - Listar um registro
 app.get("/todos/:id", (request, response) => {
-  const { id } = request.params;
+console.log("GET-único");
 
-  const todoIndex = todos.findIndex((todo) => todo.id === id);
+  const { id } = request.params
 
-  if (todoIndex < 0) {
-    return response.status(404).json("Usuário não encontrado");
+  const todoFind = todos.find((todo)=>{
+    return todo.id === id
+  })
+
+  if (todoFind === undefined) {
+    return response.json("Registro não encontrado.");
   }
 
-  todos[todoIndex] = { id, text, done };
-
-  return response.json({ id, text, done });
+  return response.json(todoFind);
 });
 
-app.post("/todos", (request, response) => {
-  const { text, done } = request.body;
-
-  const todo = { id: v4(), text, done };
-
-  todos.push(todo);
-
-  return response.json(todo);
-});
-
+//PUT - alterar/atualiza registros
 app.put("/todos/:id", (request, response) => {
+  console.log("PUT");
   const { id } = request.params;
   const { text, done } = request.body;
 
-  const todoIndex = todos.findIndex((todo) => todo.id === id);
+  const todoIndex = todos.findIndex((todo)=> todo.id === id);
+
+console.log(todoIndex)
 
   if (todoIndex < 0) {
-    return response.status(404).json("Usuário não encontrado");
+    return response.json("Registro não encontrado.");
   }
 
   todos[todoIndex] = { id, text, done };
 
-  return response.json({ id, text, done });
+  return response.json(todos[todoIndex]);
 });
 
-app.delete("/todos/:id", function (request, response) {
-  const { id } = request.params;
+//POST - insere um registro
+app.post("/todos", (request, response) => {
+const { text, done } = request.body;
+
+const todo = {id: v4(), text, done};
+
+todos.push(todo);
+
+return response.json(`Usuário inserido com sucesso. Com o id: ${todo.id}`);
+});
+
+//DELETE - remove um registro
+app.delete("/todos/:id", (resquest, response) => {
+  const { id } = resquest.params;
 
   const todoIndex = todos.findIndex((todo) => todo.id === id);
 
-  if (todoIndex < 0) {
-    return response.status(404).json("Usuário não encontrado");
+  if(todoIndex < 0){
+    return response.status(404).json("Registro não foi encontrado.")
   }
 
   todos.splice(todoIndex, 1);
 
-  return response.status(204).send();
+  return response.status(204);
 });
 
+
+const port = 4040
+
 http
-  .createServer(app)
-  .listen(3232, () => console.log("Servidor rodando na porta 3232"));
+.createServer(app)
+.listen(port, () => console.log(`Servidor Rodando na porta ${port}`));
